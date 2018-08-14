1061aa3f645cdf4e57a53c8978392904035700b0
pipegomez05 <36645566+pipegomez05@users.noreply.github.com>
2018-08-14 02:05:29 -0500

    test12

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const os = require('os')
 const app = express()
 
 app.get('/', function (req, res) {
-	res.send('Hello World from tester11 on' + os.hostname())
+	res.send('Hello World from tester12 on' + os.hostname())
 })
 
 app.get('/healthcheck', function (req, res) {
